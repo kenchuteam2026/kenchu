5f198d1b2c19c66e55536e84f663a8ce904f4e62
kenchuteam2026 <kenchuofficialteam@proton.me>
2026-03-04 13:25:47 +0100

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -8,3 +8,22 @@ window.addEventListener("scroll", function() {
 document.getElementById("buyBtn").addEventListener("click", function() {
     alert("Redirigiendo a la compra...");
 });
+
+
+// Mostrar botón al hacer scroll
+window.onscroll = function () {
+    const btn = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+// Subir arriba al hacer click
+document.getElementById("scrollTopBtn").onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
